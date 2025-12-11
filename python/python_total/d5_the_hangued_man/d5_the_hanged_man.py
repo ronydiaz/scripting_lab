777bec18e3d4461a916779bfa085c764bdc47386
ronydiaz <ronny.diiazparedes@gmail.com>
2025-12-10 23:39:20 -0600

    Starting Hanging man

---
+++ python/python_total/d5_the_hangued_man/d5_the_hanged_man.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from random import *
+
+with open('words.txt','r') as file:
+    data = list(file.readlines())
+
+#print(data)
+
+flag = True
+
+word = choice(data)
+word = word.lower()
+word_copy = "_ " * len(word)
+print(f"Your word is: {word_copy}")
+
+live_counter = len(word)
+
+while(flag and live_counter > 0):
+    
+    char = input("Summit a letter: ")
+    
+    if "char" in word:
+        pass
+
+    else:
+        live_counter -= 1
+
+    if live_counter == 0:
+        print("You lost")
+    else:
+        print(f"Missing Lifes: {live_counter}")
