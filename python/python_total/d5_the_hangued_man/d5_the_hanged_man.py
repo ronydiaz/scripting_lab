2c6d44432309a81680c8a0ffcf97b4c56cd9bf8a
ronydiaz <ronny.diiazparedes@gmail.com>
2026-03-17 01:57:26 -0600

    Replacing underscore

--- python/python_total/d5_the_hangued_man/d5_the_hanged_man.py
+++ python/python_total/d5_the_hangued_man/d5_the_hanged_man.py
@@ -12,21 +12,29 @@ flag = True
 word = choice(data)
 word =  word.lower()
 word_copy = "_ " * len(word)
-print(f"Your word is: {word_copy}")
+print(f"Your word is: {word}")
 
 life_counter = len(word)
 
-while(flag and live_counter > 0):
+while(flag and life_counter > 0):
     
     char = input("Summit a letter: ")
     
-    if "char" in word:
-        pass
+    if char in word:
+        
+        indexes = [i for i, x in enumerate(word) if x == char]
+        print(indexes)
+
+        for i in indexes:
+            word_copy = word_copy[:i+1] + word[i] + word_copy[i:]
+
+        print(word_copy)
+
 
     else:
-        live_counter -= 1
+        life_counter -= 1
 
-    if live_counter == 0:
+    if life_counter == 0:
         print("You lost")
     else:
-        print(f"Missing Lifes: {live_counter}")
+        print(f"Missing Lifes: {life_counter}")
