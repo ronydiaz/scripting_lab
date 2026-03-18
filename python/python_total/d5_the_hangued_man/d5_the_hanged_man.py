77210d84be29e3b6b0025a44cb16f0d6d8bb0a97
ronydiaz <ronny.diiazparedes@gmail.com>
2026-03-18 00:51:59 -0600

    Hanger Man playgame

--- python/python_total/d5_the_hangued_man/d5_the_hanged_man.py
+++ python/python_total/d5_the_hangued_man/d5_the_hanged_man.py
@@ -10,9 +10,9 @@ with open('words.txt','r') as file:
 flag = True
 
 word = choice(data)
-word =  word.lower()
-word_copy = "_ " * len(word)
-print(f"Your word is: {word}")
+word =  word.lower().replace('\n','')
+word_copy = "_" * (len(word) - 1)
+print(f"Your word is: {word_copy}")
 
 life_counter = len(word)
 
@@ -20,21 +20,26 @@ while(flag and life_counter > 0):
     
     char = input("Summit a letter: ")
     
-    if char in word:
-        
+    if (char in word) and (char not in word_copy): 
+
         indexes = [i for i, x in enumerate(word) if x == char]
-        print(indexes)
+        #print(indexes)
 
         for i in indexes:
-            word_copy = word_copy[:i+1] + word[i] + word_copy[i:]
+            word_copy = word_copy[:i] + word[i] + word_copy[i+1:]
 
-        print(word_copy)
+        print("\n",word_copy,"\n")
+
+        if word_copy == word:
+            print("YOU WIN!!!\n")
 
 
     else:
         life_counter -= 1
 
+
     if life_counter == 0:
-        print("You lost")
+        print("You lost, the word is {word}")
+
     else:
         print(f"Missing Lifes: {life_counter}")
